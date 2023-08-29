bb1a9822bc6f4561a0e2e29ee1e6a4b4914a24d4
Ubaid Ullah <ubaid332@yahoo.com>
2023-08-29 18:00:45 +0500

    added professor custom post type

--- single-professor.php
+++ single-professor.php
@@ -14,14 +14,17 @@ while(have_posts()){
     </div>
 
     <div class="container container--narrow page-section">
-    <div class="metabox metabox--position-up metabox--with-home-link">
-        <p>
-          <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Events Home </a> <span class="metabox__main"><?php echo the_title(); ?></span>
-        </p>
-      </div>
+    
 
       <div class="generic-content">
-        <?php echo the_content(); ?>
+        <div class="row group">
+            <div class="one-third">
+    <?php the_post_thumbnail(); ?>
+            </div>
+            <div class="two-thirds">
+        <?php the_content(); ?>
+            </div>
+        </div>
       </div>
       
       <?php 
@@ -29,7 +32,7 @@ while(have_posts()){
       $relatedPrograms = get_field('related_programs');
       if($relatedPrograms){
         echo "<hr class='section-break'>";
-        echo "<h2 class='headline headline--medium'>Related Programs</h2>";
+        echo "<h2 class='headline headline--medium'>Subject(s) Taugth</h2>";
         echo "<ul class='link-list min-list'>";
         foreach($relatedPrograms as $program){ ?>
           <li><a href="<?php echo get_the_permalink($program) ?>"><?php echo get_the_title($program) ?></a></li>
