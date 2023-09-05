748e18e7b03e2d29e680ed1d14ac610eaff8d1bf
Ubaid Ullah <ubaid332@yahoo.com>
2023-09-05 16:42:40 +0500

    added image_sizes for professor and pageBanner

--- single-professor.php
+++ single-professor.php
@@ -4,11 +4,11 @@ get_header();
 while(have_posts()){
     the_post(); ?>
     <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
+      <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image'); echo $pageBannerImage['sizes']['pageBanner'] ?>)"></div>
       <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php echo the_title(); ?></h1>
         <div class="page-banner__intro">
-          <p>DON'T FORGET ME TO REPLACE LATER</p>
+          <p><?php echo the_field('page_banner_subtitle') ?></p>
         </div>
       </div>
     </div>
@@ -19,7 +19,7 @@ while(have_posts()){
       <div class="generic-content">
         <div class="row group">
             <div class="one-third">
-    <?php the_post_thumbnail(); ?>
+    <?php the_post_thumbnail('professorPortrait'); ?>
             </div>
             <div class="two-thirds">
         <?php the_content(); ?>
