4d42ea3c94d2189eeaefa37f9950582f2d0d2d79
Ubaid Ullah <ubaid332@yahoo.com>
2023-09-12 13:01:02 +0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -87,7 +87,7 @@ function university_adjust_queries($query){
 add_action('pre_get_posts', 'university_adjust_queries');
 
 function universityMapKey($api){
-    $api['key'] = "AIzaSyBxba9lmJoqr6iZzZVQddC92SbvuSYaQok";
+    $api['key'] = "";
     return $api;
 }
 
